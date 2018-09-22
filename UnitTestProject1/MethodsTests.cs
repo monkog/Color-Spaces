76cb3c452454dd74f72fef5ca0e3082b43b1a337
monkog <monkog@wp.pl>
2018-09-22 14:37:29 +0200

    Add test project

---
+++ UnitTestProject1/MethodsTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+	[TestFixture]
+	public class MethodsTests
+	{
+		[Test]
+		public void TestMethod1()
+		{
+			Assert.True(true);
+		}
+	}
+}
